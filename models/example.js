5903b87fe2ccec1a3297218dc555c2496d9e733e
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-01-27 20:45:20 -0800

    gitignore, config.json, model-Artist

--- models/example.js
+++ models/example.js
@@ -1,7 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var Artist = sequelize.define("Artist", {
+    Name: DataTypes.STRING,
+    Specialization: DataTypes.STRING,
+    Gender: DataTypes.STRING,
+    Price: DataTypes.INTEGER,
+    Location: DataTypes.STRING,
+    SocialMedia: DataTypes.STRING
   });
-  return Example;
+  
+  return Artist;
 };
