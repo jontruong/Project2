505e513425794725f0e7bf7d749e8718e9abecab
Jonathan Truong <jjtruong90@gmail.com>
2020-02-01 09:54:26 -0800

    Updates routes and html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,24 @@
-var db = require("../models");
+var path = require("path");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Artist.findAll({}).then(function(dbArtist) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+// Routes
+// // =============================================================
+// module.exports = function(app) {
 
-  // Load example page and pass in an example by id
-  app.get("/artists/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+//   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
+//   // index route loads view.html
+//   app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/blog.html"));
+//   });
+
+//   app.get("/cms", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/cms.html"));
+//   });
+
+//   // blog route loads blog.html
+//   app.get("/blog", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/blog.html"));
+//   });
+
+// };
+
