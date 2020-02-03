4fdffb17cd0e2a248caa682130c4e9de6bd9f286
Jonathan Truong <jjtruong90@gmail.com>
2020-02-03 10:42:18 -0800

    Adds htmls for specialization

---
+++ public/js/american.js
@@ -0,0 +1,21 @@
+$.get("/api/artists/american", function(data) {
+
+    // For each book that our server sends us back
+    for (var i = 0; i < data.length; i++) {
+      // Create a parent div to hold book data
+      var wellSection = $("<div>");
+      // Add a class to this div: 'well'
+      wellSection.addClass("well");
+      // Add an id to the well to mark which well it is
+      wellSection.attr("id", "specialization-well-" + i);
+      // Append the well to the well section
+      $("#well-section").append(wellSection);
+  
+      // Now  we add our book data to the well we just placed on the page
+      $("#specialization-well-" + i).append("<h2>" + (i + 1) + ". " + data[i].name + "</h2>");
+      $("#specialization-well-" + i).append("<h3>Author: " + data[i].gender + "</h4>");
+      $("#specialization-well-" + i).append("<h3>Genre: " + data[i].location + "</h4>");
+      $("#specialization-well-" + i).append("<h3>Pages: " + data[i].price + "</h4>");
+      $("#specialization-well-" + i).append("<h3>Pages: " + data[i].socialMedia + "</h4>");
+    }
+  });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -26,6 +26,33 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "/../public/register.html"));
   });
 
+  app.get("/american", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/american.html"));
+  });
+
+  app.get("/japanese", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/japanese.html"));
+  });
+
+  app.get("/blackwork", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/blackwork.html"));
+  });
+
+  app.get("/realism", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/realism.html"));
+  });
+
+  app.get("/portrait", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/portrait.html"));
+  });
+
+  app.get("/newschool", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/newschool.html"));
+  });
+
+  app.get("/tribal", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/tribal.html"));
+  });
   // If no matching route is found default to home
   app.use(function(req,res){
     res.sendFile(path.join(__dirname, "/../public/home.html"));
